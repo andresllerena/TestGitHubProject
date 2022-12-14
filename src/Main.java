d3f85db889384a0aae50f6c9bffc3ab296e8e0a5
andyllerena <andres.g.llerena@gmail.com>
2022-12-13 23:12:29 -0500

    Update via terminal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Update");
+        System.out.println("Hello world! Update (will submit via terminal)");
     }
 }
